5b905ebb052498aaa3f62f7a61d5c31e902c16fe
Dan Bishop <d@nbishop.uk>
2023-10-25 09:46:02 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+gem "bootstrap", "~> 5.3"
+
+gem "sinatra", "~> 3.1"
+
+gem "font-awesome-sass", "~> 6.4"
---
+++ build_assets.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'fileutils'
+require 'bootstrap'
+require 'font-awesome-sass'
+require 'sassc'
+
+# Stylesheets
+bootstrap_css = "#{Bootstrap.assets_path}/stylesheets/_bootstrap.scss"
+fontawesome_css = "#{FontAwesome::Sass.assets_path}/stylesheets/_font-awesome.scss"
+
+scss = "@import './lib/custom.scss';@import '#{bootstrap_css}';@import '#{fontawesome_css}';"
+
+File.write('./public/css/stylesheet.css', SassC::Engine.new(scss, style: :compressed).render)
+
+# Fonts
+font_folder = './public/webfonts/'
+fontawesome_fonts = Dir["#{FontAwesome::Sass.assets_path}/fonts/font-awesome/*"]
+fontawesome_fonts.each do |filename|
+  FileUtils.cp(filename, font_folder)
+end
+
+# Scripts
+scripts_folder = './public/js/'
+bootstrap_js = "#{Bootstrap.assets_path}/javascripts/bootstrap.min.js"
+FileUtils.cp(bootstrap_js, scripts_folder)
